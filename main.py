6b6e2b2f480631f3cf5b05e020d60f4ba2009a52
Ryan Saeta <rsaeta@gmail.com>
2023-11-09 19:03:21 +0100

    Using BERT seems nicer

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import torch
-
+from typing import List
 import os
 from pathlib import Path
-from transformers import LongformerModel, LongformerTokenizerFast
+from transformers import LongformerModel, LongformerTokenizerFast, BertModel, BertTokenizerFast
+
+
+def cross_similarity(reps: List[torch.Tensor]):
+    """
+    Computes the cosine similarity between all pairs of representations in reps
+    """
+    sims = []
+    for i, rep1 in enumerate(reps):
+        for j, rep2 in enumerate(reps):
+            if i == j:
+                continue
+            sims.append(torch.cosine_similarity(rep1, rep2))
+    return torch.stack(sims)
 
 
 def sim_matrix(a, b, eps=1e-8):
@@ -15,21 +28,28 @@ def sim_matrix(a, b, eps=1e-8):
     sim_mt = torch.mm(a_norm, b_norm.transpose(0, 1))
     return sim_mt
 
+
 def main():
+    """
     model_name = 'allenai/longformer-base-4096'
     tokenizer = LongformerTokenizerFast.from_pretrained(model_name)
     model = LongformerModel.from_pretrained(model_name)
+    """
+    model_name = 'bert-base-uncased'
+    tokenizer = BertTokenizerFast.from_pretrained(model_name)
+    model = BertModel.from_pretrained(model_name)
 
     job_files = os.listdir('data')
     results = []
     for fn in job_files:
         with open(Path('data') / fn) as f:
             description = f.read()
-            tokens = tokenizer(description, return_tensors='pt')
+            tokens = tokenizer(description, return_tensors='pt', padding=True, truncation=True)
             outputs = model(**tokens)
             results.append(outputs.pooler_output)
 
-    print(torch.cosine_similarity(results[0], results[1]))
+    print(job_files)
+    print(cross_similarity(results))
     print('Done')
 
 
